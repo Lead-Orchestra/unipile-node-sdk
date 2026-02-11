ef10c6237ac34cf43db2b6a84fcea49d0de9be71
TechWithTy <tyrique1.daniel@gmail.com>
2026-02-11 09:10:31 -0700

    fix: syntax error

--- src/resources/extra_params.spec.ts
+++ src/resources/extra_params.spec.ts
@@ -33,114 +33,99 @@ describe('extra_params', () => {
 	//----------------------------------------------------------------------------
 	describe('GET', () => {
 		//--------------------------------------------------------------------------
-		it(
-			'should inject extra_params as query string ' + 'on GET ' + 'when no input params',
-			async () => {
-				const r = await client.account.getOne('some_account_id', {
-					extra_params: { xtra: 'test' },
-				});
-				//   console.log(r);
-				expect((r as any).url).toBe(API_URL + 'accounts/some_account_id?xtra=test');
-			}
-		);
+		it('should inject extra_params as query string ' +
+			'on GET ' +
+			'when no input params', async () => {
+			const r = await client.account.getOne('some_account_id', {
+				extra_params: { xtra: 'test' },
+			});
+			//   console.log(r);
+			expect((r as any).url).toBe(API_URL + 'accounts/some_account_id?xtra=test');
+		});
 		//--------------------------------------------------------------------------
-		it(
-			'should inject extra_params as query string ' +
-				'and not overwrite input params ' +
-				'on GET ' +
-				'when mixed with input params',
-			async () => {
-				const r = await client.account.getAll(
-					{ limit: 30 },
-					{ extra_params: { limit: 'garbage', legit: 'test' } }
-				);
-				//   console.log(r);
-				expect((r as any).url).toBe(API_URL + 'accounts?limit=30&legit=test');
-			}
-		);
+		it('should inject extra_params as query string ' +
+			'and not overwrite input params ' +
+			'on GET ' +
+			'when mixed with input params', async () => {
+			const r = await client.account.getAll(
+				{ limit: 30 },
+				{ extra_params: { limit: 'garbage', legit: 'test' } }
+			);
+			//   console.log(r);
+			expect((r as any).url).toBe(API_URL + 'accounts?limit=30&legit=test');
+		});
 	});
 	//----------------------------------------------------------------------------
 	describe('POST | PUT', () => {
 		//--------------------------------------------------------------------------
-		it.skip(
-			'should inject extra_params in body ' + 'on POST ' + 'when no input params',
-			async () => {}
-		);
+		it.skip('should inject extra_params in body ' +
+			'on POST ' +
+			'when no input params', async () => {});
 		//--------------------------------------------------------------------------
-		it(
-			'should inject extra_params in body ' +
-				'and not overwrite input params ' +
-				'on POST ' +
-				'when body Content-Type is application/json ' +
-				'and mixed with input params',
-			async () => {
-				const r = await client.account.connect(
-					{ provider: 'WHATSAPP' },
-					{ extra_params: { provider: 'garbage', legit: 'test' } }
-				);
-				// console.log(r);
-				expect((r as any).url).toBe(API_URL + 'accounts');
-				expect(fetchMock.mock.calls[0]?.[1]?.body).toBe(
-					JSON.stringify({ provider: 'WHATSAPP', legit: 'test' })
-				);
-			}
-		);
+		it('should inject extra_params in body ' +
+			'and not overwrite input params ' +
+			'on POST ' +
+			'when body Content-Type is application/json ' +
+			'and mixed with input params', async () => {
+			const r = await client.account.connect(
+				{ provider: 'WHATSAPP' },
+				{ extra_params: { provider: 'garbage', legit: 'test' } }
+			);
+			// console.log(r);
+			expect((r as any).url).toBe(API_URL + 'accounts');
+			expect(fetchMock.mock.calls[0]?.[1]?.body).toBe(
+				JSON.stringify({ provider: 'WHATSAPP', legit: 'test' })
+			);
+		});
 		//--------------------------------------------------------------------------
-		it(
-			'should inject extra_params in body ' +
-				'and not overwrite input params ' +
-				'on POST ' +
-				'when body Content-Type is multipart/form-data ' +
-				'and mixed with input params',
-			async () => {
-				const r = await client.email.send(
-					{ account_id: 'some_account_id', to: [{ identifier: 'att@mail.com' }], body: 'content' },
-					{ extra_params: { account_id: 'garbage', legit: 'test' } }
-				);
+		it('should inject extra_params in body ' +
+			'and not overwrite input params ' +
+			'on POST ' +
+			'when body Content-Type is multipart/form-data ' +
+			'and mixed with input params', async () => {
+			const r = await client.email.send(
+				{ account_id: 'some_account_id', to: [{ identifier: 'att@mail.com' }], body: 'content' },
+				{ extra_params: { account_id: 'garbage', legit: 'test' } }
+			);
 
-				const expectedFormData = [
-					//   ['account_id', 'garbage'],
-					['account_id', 'some_account_id'],
-					['legit', 'test'],
-					['body', 'content'],
-					['to', '[{"identifier":"att@mail.com"}]'],
-				];
-				expect((r as any).url).toBe(API_URL + 'emails');
-				const resultFormData = [...fetchMock.mock.calls[0]?.[1]?.body.entries()];
-				// console.log(r, resultFormData);
-				expect(resultFormData).toEqual(expect.arrayContaining(expectedFormData));
-				expect(resultFormData.length).toBe(expectedFormData.length);
-			}
-		);
+			const expectedFormData = [
+				//   ['account_id', 'garbage'],
+				['account_id', 'some_account_id'],
+				['legit', 'test'],
+				['body', 'content'],
+				['to', '[{"identifier":"att@mail.com"}]'],
+			];
+			expect((r as any).url).toBe(API_URL + 'emails');
+			const resultFormData = [...fetchMock.mock.calls[0]?.[1]?.body.entries()];
+			// console.log(r, resultFormData);
+			expect(resultFormData).toEqual(expect.arrayContaining(expectedFormData));
+			expect(resultFormData.length).toBe(expectedFormData.length);
+		});
 	});
 	//----------------------------------------------------------------------------
 	describe('DELETE', () => {
 		//--------------------------------------------------------------------------
-		it(
-			'should inject extra_params as query string ' + 'on DELETE ' + 'when no input params',
-			async () => {
-				const r = await client.account.delete('some_account_id', {
-					extra_params: { xtra: 'test' },
-				});
-				//   console.log(r);
-				expect((r as any).url).toBe(API_URL + 'accounts/some_account_id?xtra=test');
-			}
-		);
+		it('should inject extra_params as query string ' +
+			'on DELETE ' +
+			'when no input params', async () => {
+			const r = await client.account.delete('some_account_id', {
+				extra_params: { xtra: 'test' },
+			});
+			//   console.log(r);
+			expect((r as any).url).toBe(API_URL + 'accounts/some_account_id?xtra=test');
+		});
 		//--------------------------------------------------------------------------
-		it(
-			'should inject extra_params as query string ' +
-				'and not overwrite input params ' +
-				'on DELETE ' +
-				'when mixed with input params',
-			async () => {
-				const r = await client.email.delete.byProviderId('email-id', 'some_account_id', {
-					extra_params: { account_id: 'garbage', legit: 'test' },
-				});
-				// console.log(r);
-				expect((r as any).url).toBe(
-					API_URL + 'emails/email-id?account_id=some_account_id&legit=test'
-				);
-			}
-		);
+		it('should inject extra_params as query string ' +
+			'and not overwrite input params ' +
+			'on DELETE ' +
+			'when mixed with input params', async () => {
+			const r = await client.email.delete.byProviderId('email-id', 'some_account_id', {
+				extra_params: { account_id: 'garbage', legit: 'test' },
+			});
+			// console.log(r);
+			expect((r as any).url).toBe(
+				API_URL + 'emails/email-id?account_id=some_account_id&legit=test'
+			);
+		});
 	});
 });
